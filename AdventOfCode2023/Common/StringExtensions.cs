9b130ed63968604c616b7e00f37a548fc49af670
Brendan Caffrey <brendan@devspade.com>
2023-12-01 14:08:36 -0500

    comment string extensions

--- AdventOfCode2023/Common/StringExtensions.cs
+++ AdventOfCode2023/Common/StringExtensions.cs
@@ -3,6 +3,13 @@ namespace AdventOfCode2023.Common
 {
 	public static class StringExtensions
 	{
+        /// <summary>
+        /// Replace the first instance of a string inside another string eg "helloqwerty".Replace("e","X") => "hXlloquerty"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="search"></param>
+        /// <param name="replace"></param>
+        /// <returns></returns>
         public static string ReplaceFirst(this string text, string search, string replace)
         {
             int pos = text.IndexOf(search);
@@ -13,6 +20,12 @@ namespace AdventOfCode2023.Common
             return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
         }
 
+
+        /// <summary>
+        /// Reverse a string - eg "abc" becomes "cba"
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static string Reverse( this string s)
         {
             char[] charArray = s.ToCharArray();
